468112455e51999c71fffb83b10a159c9cf242d6
p4vel <mczerny@gmx.de>
2014-06-15 23:55:04 +0200

    first steps, echo, commenting

--- index.php
+++ index.php
@@ -4,7 +4,20 @@
 		<title>Hello World</title>
 	</head>
 	<body>
-	<?php echo "Hello World"; ?>
+	<?php
+	// single line comments
+	# or like this 
+	
+	/*
+		multi
+		line
+		comments
+	*/
+	?>
+	<?php echo "Hello World!"; ?><br />
+	<?php print "Hello World!"?><br />
+	<?php echo "Hello " . "World!"?><br />
+
+	<?php echo 4+2; ?>
 	</body>
 </html>
-
