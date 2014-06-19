9f621d1a46e7bfdfe180ccbe248ae4a1b73cff84
p4vel <mczerny@gmx.de>
2014-06-20 01:05:33 +0200

    constants.php added

---
+++ constants.php
@@ -0,0 +1,17 @@
+<html>
+	<head>
+		<title>constants</title>
+	</head>
+	<body>
+	<?php  
+		$max_width = 980;
+		define("MAX_WIDTH", 980);
+
+		echo MAX_WIDTH . "<br />";
+
+		$max_width += 1;
+
+		echo $max_width;
+	?>
+	</body>
+</html>
