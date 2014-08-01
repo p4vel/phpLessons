82a146d644e2d86af414688bf1a747cbbfeb8b17
unknown <mczerny@ATL-DRESDEN.atlantisgmbh.local>
2014-08-01 13:39:50 +0200

    add_subjects

--- widget_corp/includes/functions.php
+++ widget_corp/includes/functions.php
@@ -77,15 +77,15 @@
 	}
 
 	// navigation takes 2 arguments
-	// - the currently selected subject ID (if any)
-	// - the currently selected page ID (if any)
-	function navigation($subject_id, $page_id)
+	// - the current subject array or null
+	// - the currently page array or null
+	function navigation($subject_array, $page_array)
 	{
 		$output = "<ul class=\"subjects\">";
 		$subject_set = find_all_subjects();
 		while ($subject = mysqli_fetch_assoc($subject_set)) {
 			$output .= "<li";
-			if ($subject_id == $subject["id"]) {
+			if ($subject_array && $subject["id"] == $subject_array["id"]) {
 					$output .= " class=\"selected\"";
 				}
 				$output .= ">";
@@ -98,7 +98,7 @@
 						$page_set = find_pages_for_subject($subject["id"]);
 						while ($page = mysqli_fetch_assoc($page_set)) {
 							$output .= "<li ";
-							if ($page["id"] == $page_id) {
+							if ($page_array["id"] && $page["id"] == $page_array["id"]) {
 								$output .= "class=\"selected\"";
 							}
 							$output .= ">";
@@ -117,5 +117,22 @@
 		$output .= "</ul>";
 
 		return $output;
+	}
+
+	function find_selected_page()
+	{
+		global $current_subject;
+		global $current_page;
+		if(isset($_GET["subject"])) {
+			$current_subject = find_subject_by_id($_GET["subject"]);
+			$current_page = null;
+		} elseif (isset($_GET["page"])) {
+			$current_subject = null;
+			$current_page = find_page_by_id($_GET["page"]);
+		} else {
+			$current_subject = null;
+			$current_page = null;
+		}
+		// return true;
 	}
 ?>
--- widget_corp/public/manage_content.php
+++ widget_corp/public/manage_content.php
@@ -3,31 +3,17 @@
 <?php require_once '../includes/functions.php'; ?>
 <?php include '../includes/layouts/header.php'; ?>
 
-<?php 
-	if(isset($_GET["subject"])) {
-		$selected_subject_id = $_GET["subject"];
-		$selected_page_id = null;
-	} elseif (isset($_GET["page"])) {
-		$selected_subject_id = null;
-		$selected_page_id = $_GET["page"];
-	} else {
-		$selected_page_id = null;
-		$selected_subject_id = null;
-	}
-?>
-
 <div id="main">
 	<div id="navigation">
-		<?php  echo navigation($selected_subject_id, $selected_page_id);  ?>
+		<?php  echo navigation($current_subject, $current_page);  ?>
+		<br />
+		<a href="new_subject.php">Add a subject</a>
 	</div>
 	<div id="page">
 		<h2>Manage Content</h2>
-		<?php if ($selected_subject_id) {  ?>
-		<?php 		$current_subject = find_subject_by_id($selected_subject_id) ?>
+		<?php if ($current_subject) {  ?>
 					Subject name: <?php echo $current_subject["menu_name"]; ?>
-
-		<?php } elseif ($selected_page_id) { ?>
-		<?php 		$current_page = find_page_by_id($selected_page_id) ?>
+		<?php } elseif ($current_page) { ?>
 					Page Name: <?php  echo $current_page["menu_name"]; ?>
 		<?php } else { ?>
 					Please select a subject or a page
