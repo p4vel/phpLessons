c04402f0b74cc9c80a95070835d483bea0f6ad08
unknown <mczerny@ATL-DRESDEN.atlantisgmbh.local>
2014-08-18 12:12:14 +0200

    admin-managing added

--- widget_corp/includes/functions.php
+++ widget_corp/includes/functions.php
@@ -35,6 +35,38 @@
 		return $output;
 	}
 
+	function find_all_admins(){
+		global $connection;
+		$query  = "SELECT * ";
+		$query .= "FROM admins ";
+		$query .= "ORDER BY username ASC;";
+		$admin_set = mysqli_query($connection, $query);
+		print_r($connection);
+		confirm_query($admin_set);
+		return $admin_set;
+	}
+
+	function find_admin_by_id($admin_id)
+	{
+		global $connection;
+
+		$safe_subject_id = mysqli_real_escape_string($connection, $subject_id);
+
+		$query  = "SELECT * ";
+		$query .= "FROM admins ";
+		$query .= "WHERE id = {$safe_admin_id} ";
+		$query .= "LIMIT 1";
+		$admin_set = mysqli_query($connection, $query);
+		
+		confirm_query($admin_set);
+
+		if ($admin = mysqli_fetch_assoc($admin_set)){
+			return $admin;
+		} else {
+			return null;
+		}
+	}
+
 	function find_all_subjects($public=true){
 		global $connection;
 		$query  = "SELECT * ";
---
+++ widget_corp/public/manage_admins.php
@@ -0,0 +1,40 @@
+<?php require_once '../includes/session.php'; ?>
+<?php require_once '../includes/db_connection.php'; ?>
+<?php require_once '../includes/functions.php'; ?>
+
+<?php 
+	$admin_set = find_all_admins();
+	print_r($admin_set);
+?>
+
+<?php $layout_context = 'admin'; ?>
+<?php include '../includes/layouts/header.php'; ?> 
+
+<div id="main">
+	<div id="navigation">
+		&nbsp;
+	</div>
+	<div id="page">
+		<?php echo message(); ?>
+
+		<h2>Manage Admins</h2>
+
+		<table>
+			<tr>
+				<th>Username</th>
+				<th>Actions</th>
+			</tr>
+			<?php while ($admin = mysql_fetch_assoc($admin_set)) { ?>
+				<tr>
+					<td><?php echo htmlentities($admin["username"]); ?></td>
+					<td>
+						<a href="edit_admin.php?id=<?php echo urlencode($admin["id"]);?>">Edit</a>
+						<a href="delete_admin.php?id=<?php echo urlencode($admin["id"]);?>">Delete</a>
+					</td>
+				</tr>	
+			<?php } ?>
+		</table>
+		<br />
+		<a href="new_Admin.php">Add admin</a>
+	</div>
+<?php include '../includes/layouts/footer.php'; ?>
