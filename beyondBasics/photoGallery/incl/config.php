c1b80446873270d4700cfde3309557199dcbdabb
p4vel <mczerny@gmx.de>
2014-09-01 00:50:57 +0200

    project founded, intialized

---
+++ beyondBasics/photoGallery/incl/config.php
@@ -0,0 +1,13 @@
+<?php 
+
+	defined("DB_SERVER") ? null : define("DB_SERVER", "db537709704.db.1and1.com");
+	defined("DB_USER") ? null : define("DB_USER", "dbo537709704");
+	defined("DB_PASS") ? null : define("DB_PASS", "drexakQ2Llr?");
+	defined("DB_NAME") ? null : define("DB_NAME", "db537709704");
+	// 1. Create a database connection
+	// $dbhost = "db537709704.db.1and1.com";
+	// $dbuser = "dbo537709704";
+	// $dbpass = "drexakQ2Llr?";
+	// $dbname = "db537709704";
+
+ ?>
---
+++ beyondBasics/photoGallery/public/index.php
@@ -0,0 +1,5 @@
+<?php 
+
+echo "IS this working";
+
+?>
