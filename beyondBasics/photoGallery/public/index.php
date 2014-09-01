6f5f3f44917b6697bec38584d495f890e873bb93
p4vel <mczerny@gmx.de>
2014-09-02 01:01:11 +0200

    User class erstellt

--- beyondBasics/photoGallery/public/index.php
+++ beyondBasics/photoGallery/public/index.php
@@ -1,21 +1,22 @@
 <?php 
 require_once("../incl/database.php");
-
-if (isset($database)) {echo "true";} else { echo "false";}
-// equals: echo (isset($database)) ? "true" : "false";
-
-echo "<br />";
-
-echo $database->escape_value("It's working?<br />");
+require_once("../incl/user.php");
 
 
-//$sql  = "INSERT INTO users (id, username, password, first_name, last_name) ";
-//$sql .= "VALUES (1, 'mczerny', 'start123', 'Mirko', 'Czerny')";
-//$result = $database->query($sql);
+$user = User::find_by_id(1);
+// echo "<pre>";
+// var_dump($user);
+// echo "</pre>";
+echo $user->full_name(); 
 
-$sql = "SELECT * FROM users WHERE id = 1";
-$result_set = $database->query($sql);
-$found_user = $database->fetch_array($result_set);
-echo $found_user['username'];
+echo "<hr />";
+
+$users = User::find_all();
+
+foreach ($users as $user){
+	echo "User: " . $user->username . "<br />" ;
+	echo "Name: " . $user->full_name() . "<br /><br />";
+}
+
 
 ?>
