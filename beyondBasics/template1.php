c3ce6a9e89c1a0863b6b2daa35d4bf63ec6f4b18
p4vel <mczerny@gmx.de>
2014-10-06 17:42:02 +0200

    templating, MVC introduction

---
+++ beyondBasics/template1.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title><?php echo $page_title; ?></title>
+	</head>
+	<body>
+
+	<h1><?php echo $page_title; ?></h1>
+
+	<p><?php echo $conent; ?></p>
+
+	</body>
+</html>
---
+++ beyondBasics/template_variable.php
@@ -0,0 +1,8 @@
+<?php 
+
+	$page_title = "Template Test";
+	$content = "This is a test of templating using variables.";
+
+	include('template1.php');
+
+?>
